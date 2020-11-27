0345f29c46452f5a3f7bdaf5a0db5b21c5277990
Scott Maness <scottman1987@ymail.com>
2020-11-27 09:37:22 -0600

    lesson 31 to lesson 38

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public AccountController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // Lesson 35: Initial creation
+        // Lesson 36: (user exists checking and change to RegisterDto)
+        [HttpPost("register")]
+        public async Task<ActionResult<AppUser>> RegisterAsync(RegisterDto registerDto)
+        {
+            if(await UserExists(registerDto.Username)) return BadRequest("Username already taken");
+
+            // HMACSHA512 is IDisposable
+            using var hmac = new HMACSHA512();
+
+            var user = new AppUser
+            {
+                UserName = registerDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+        }
+    }
+}
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -7,5 +7,22 @@ namespace API.Entities
 
         // EF note: Use this as written, with case!
         public string UserName { get; set; }
+        
+        // Lesson 33:
+        // we're doing passwords in a way that isn't
+        // secure enough for modern use, but will
+        // help us understand how things work.
+        // Later we'll upgrade this.
+        //
+        // we run 
+        // dotnet ef migrations add UserPasswordAdded
+        // in the terminal to pick up these new fields in 
+        // the ORM.
+        // 
+        // Then:
+        // dotnet ef database update
+        // also in the terminal
+        public byte[] PasswordHash{get;set;}
+        public byte[] PasswordSalt{get;set;}
     }
 }
